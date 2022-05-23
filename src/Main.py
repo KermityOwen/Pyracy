a2b11e59f48ec58d276a4e44ea7e277848e6de15
Owen Lee <owentheturkey@gmail.com>
2022-05-23 18:06:07 +0100

    Fixed a ton of stuff and created a distributable
    TOOK WAY TOO MF LONG PATHING AND STUFF IS HARD AND PYINSTALLER IS SO UNITUITIVE FOR EXTERNAL DEPENDENCIES

--- src/Main.py
+++ src/Main.py
@@ -4,18 +4,21 @@ import Downloader
 import json
 import os
 
-if __name__ == "__main__":
-    # ~~~ MAIN ~~~ #
-    base_path = os.path.dirname(__file__)
-    config_path = os.path.join(base_path, "..", "config.json")
+""" ~~~ MAIN ~~~ """
+config_path = os.path.abspath("../config.json")
 
-    with open(config_path, "r", encoding="utf-8") as f:
-        config = json.load(f)
+with open(config_path, "r", encoding="utf-8") as f:
+    config = json.load(f)
 
-    search_url = Searcher.compile_search_url(str(input("Search: ")))
-    htmlsrc = Searcher.get_html_as_string(search_url)
-    urls = Searcher.filter_video_links(htmlsrc, 10)
-    yts = Processor.map_choices(urls)
-    Processor.display_titles(yts)
-    ystream = Processor.specify_video(yts, int(input("Choice: ")) - 1)
-    Downloader.download_video(ystream, config["dl_path"])
+search_url = Searcher.compile_search_url(str(input("Search: ")))
+print("---")
+htmlsrc = Searcher.get_html_as_string(search_url)
+urls = Searcher.filter_video_links(htmlsrc, 10)
+yts = Processor.map_choices(urls)
+Processor.display_titles(yts)
+print("---")
+ystream = Processor.specify_video(yts, int(input("Choice: ")) - 1)
+print("---")
+Downloader.download_video(ystream, config["dl_path"])
+
+input("\nDownloaded. Press ENTER to end program.")
---
+++ src/build.bat
@@ -0,0 +1,2 @@
+python -m PyInstaller --onefile --hidden-import=pytube --add-data=../config.json;config --name=Pyracy -i=../assets/images/pyracy-icon.ico --clean Main.py
+cmd /k
